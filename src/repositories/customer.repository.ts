11c17efe096b8453dee77600a2a60e6709c706ec
gabrielalmir <gabr.almir@gmail.com>
2024-11-11 16:43:56 -0300

    feat: simplify repository search

--- src/repositories/customer.repository.ts
+++ src/repositories/customer.repository.ts
@@ -1,25 +1,18 @@
 import { api } from "@/lib/api";
 import { Customer } from "@/models/customer";
-import { z } from "zod";
 
-const customerQuerySchema = z.object({
-    nome: z.string().optional(),
-    grupo: z.string().optional(),
-    limite: z.coerce.number().optional(),
-    pagina: z.coerce.number().optional(),
-});
+interface CustomerQuerySchema {
+    nome?: string;
+    grupo?: string;
+    limite?: number;
+    pagina?: number;
+}
 
 export class CustomerRepository {
     private readonly endpoint = 'clientes';
 
-    async search(query: z.infer<typeof customerQuerySchema>): Promise<Customer[]> {
-        const { data, error } = customerQuerySchema.safeParse(query);
-
-        if (error) {
-            throw new Error(`Invalid query: ${error.errors}`);
-        }
-
-        const response = await api.get(this.endpoint, { params: data });
+    async search(query: CustomerQuerySchema): Promise<Customer[]> {
+        const response = await api.get(this.endpoint, { params: query });
         return response.data;
     }
 }
--- src/repositories/product.repository.ts
+++ src/repositories/product.repository.ts
@@ -1,25 +1,18 @@
 import { api } from "@/lib/api";
 import { Product } from "@/models/product";
-import { z } from "zod";
 
-const productQuerySchema = z.object({
-    descricao: z.string().optional(),
-    grupo: z.string().optional(),
-    limite: z.coerce.number().optional(),
-    pagina: z.coerce.number().optional(),
-});
+interface ProductQuerySchema {
+    descricao?: string;
+    grupo?: string;
+    limite?: number;
+    pagina?: number;
+}
 
 export class ProductRepository {
     private readonly endpoint = 'produtos';
 
-    async search(query: z.infer<typeof productQuerySchema>): Promise<Product[]> {
-        const { data, error } = productQuerySchema.safeParse(query);
-
-        if (error) {
-            throw new Error(`Invalid query: ${error.errors}`);
-        }
-
-        const response = await api.get(this.endpoint, { params: data });
+    async search(query: ProductQuerySchema): Promise<Product[]> {
+        const response = await api.get(this.endpoint, { params: query });
         return response.data;
     }
 }

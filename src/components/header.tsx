627368399fbbfec2dad8256fb2b96c6d23e8d263
gabrielalmir <gabr.almir@gmail.com>
2025-03-25 22:15:50 -0300

    fix: update mediaTypes in ImagePicker configuration

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -32,7 +32,7 @@ export default function Header({ username }: Readonly<HeaderProps>) {
     }
 
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: 'images',
       allowsEditing: true,
       aspect: [1, 1],
       quality: 1,

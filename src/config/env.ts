d619ac745d774894e1d012f363968b0722676c00
gabrielalmir <gabr.almir@gmail.com>
2025-03-25 21:46:26 -0300

    feat: update base url

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 
 export const envSchema = z.object({
     GEMINI_API_KEY: z.string().default('AIzaSyD3DIuL66TpQCZ4-WdCpc5Fy3D0AVJ4QlI'),
-    API_BASE_URL: z.string().default('https://techfinance.fly.dev/'),
+    API_BASE_URL: z.string().default('https://techfinance-api.fly.dev/'),
     API_TOKEN: z.string().default('ronaldo'),
 });
 
